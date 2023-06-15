ecf447c0dcd7dd1cf5648c346b41a2ed3d269461
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-06-16 02:47:54 +0530

    Add tag and postandtag entities
    Post and Tag entities have many to many relationship. since extra columns are needed they are implemented with one to many and many to one bidirectional mapping with postandtag has mediator table.

---
+++ src/main/java/com/surendra/BlogApplication/model/Tag.java
@@ -0,0 +1,30 @@
+package com.surendra.BlogApplication.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+    @OneToMany(mappedBy = "tag", cascade = CascadeType.REMOVE)
+    public Set<PostAndTag> postAndTags = new HashSet<>();
+}
