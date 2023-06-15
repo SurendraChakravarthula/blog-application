ca023dd10ea59b091215a51782b1dc745b215a29
Surendra Chakravarthula <surendra.chakravarthula.24.1@mountblue.tech>
2023-06-16 02:45:24 +0530

    Create repository for post and tag
    Repositories are created that implements JPA interface to use inbuilt JPA specifications.

---
+++ src/main/java/com/surendra/BlogApplication/repository/CommentsRepository.java
@@ -0,0 +1,16 @@
+package com.surendra.BlogApplication.repository;
+
+import com.surendra.BlogApplication.model.Comment;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentsRepository extends JpaRepository<Comment, Long> {
+    @Query("DELETE FROM Comment c WHERE c.id = :id")
+    @Modifying
+    @Transactional
+    void deleteComment(long id);
+}
---
+++ src/main/java/com/surendra/BlogApplication/repository/PostsRepository.java
@@ -0,0 +1,17 @@
+package com.surendra.BlogApplication.repository;
+
+import com.surendra.BlogApplication.model.Post;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostsRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+    @Query("DELETE FROM Post p WHERE p.id = :id")
+    @Transactional
+    @Modifying
+    void deletePost(long id);
+}
